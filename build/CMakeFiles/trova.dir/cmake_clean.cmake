675cc337e25a5d1591887541067ca0d9e874e25d
ElPatatone <rahman.hm2002@gmail.com>
2023-07-10 19:42:35 +0100

    feat: made a cmake file for building the cli tool

---
+++ build/CMakeFiles/trova.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trova.dir/main.c.o"
+  "CMakeFiles/trova.dir/main.c.o.d"
+  "trova"
+  "trova.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/trova.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/trova.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for trova.
+# This may be replaced when dependencies are built.
